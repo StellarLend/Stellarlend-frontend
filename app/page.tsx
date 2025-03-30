2ad90502f4664f79ebee39d8adf5205d35849bd1
bigchills <yemi7164@gmail.com>
2025-03-30 15:17:29 +0100

    chore: clean up page component and render hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
+import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
 import Header from "@/components/Navbar/Navbar";
 import TestimonialsSection from "@/components/testimonial";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
       <Header />
       <main>
-        <HowItWorks/>
+        <Hero />
+        <HowItWorks />
         <TestimonialsSection />
       </main>
-      <footer >
-        
-      </footer>
     </div>
   );
 }
