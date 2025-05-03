31ef992de248d4446f10c8ed528af0594625b2c4
drakkomaximo <andres.villanueva.tabares@gmail.com>
2025-05-03 16:05:23 -0500

    Table ui has been created

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import TestimonialsSection from "@/components/testimonial";
 import ExploreFeatures from "@/components/ExploreFeatures";
 import Hero from "@/components/Hero";
 
-
 export default function Home() {
   return (
     <div className="">
