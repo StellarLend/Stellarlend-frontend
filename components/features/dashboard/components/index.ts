a0f4373a45d9a4ba691ff02c6ef5eb5609c5c0e1
nanaabdul1172 <abdulrahmanfirdausionize@gmail.com>
2026-06-28 10:18:14 +0100

    feat: print-friendly receipt view for transaction detail

--- components/features/dashboard/components/index.ts
+++ components/features/dashboard/components/index.ts
@@ -2,3 +2,5 @@ export { default as MetricsCards } from './MetricsCards';
 export { default as LiquidationsPanel } from './LiquidationsPanel';
 export { default as PositionSummary } from './PositionSummary';
 export { default as NextPaymentDue } from './NextPaymentDue';
+export { default as TransactionDetail } from './TransactionDetail';
+export { default as TransactionReceipt } from './TransactionReceipt';
